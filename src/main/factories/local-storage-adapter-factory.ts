b317423a089462df98b00dbad05e48f36b993c1d
lucas <lutamaribuchi@gmail.com>
2023-01-14 03:02:47 -0300

    refactor: remove local storage

--- src/main/factories/local-storage-adapter-factory.ts
+++
@@ -1,6 +0,0 @@
-import LocalStorageAdapter from '../../infra/cache/local-storage-adapter';
-
-const makeLocalStorageAdapter = (): LocalStorageAdapter =>
-  new LocalStorageAdapter();
-
-export default makeLocalStorageAdapter;
